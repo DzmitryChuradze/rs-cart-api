4e76de81cc0501c1f0000ebd58557bfc8afdaa7a
Dzmitry Churadze <Dzmitry.Churadze@refinitiv.com>
2023-04-03 18:54:58 +0200

    feat(task-8): rds

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,20 @@
+import { ClientConfig } from "pg";
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
 export const JWT_CONFIG = {
   secret: 'secret',
   expiresIn: '12h'
 }
+
+export const PG_CLIENT_CONFIG: ClientConfig = {
+  host: PG_HOST,
+  port: parseInt(PG_PORT),
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  connectionTimeoutMillis: 3000,
+  ssl: {
+    rejectUnauthorized: false
+  }
+};
--- src/order/services/order.service.ts
+++ src/order/services/order.service.ts
@@ -1,39 +1,96 @@
 import { Injectable } from '@nestjs/common';
-import { v4 } from 'uuid';
-
+import { CartService } from 'cart';
+import { Client } from 'pg';
+import { PG_CLIENT_CONFIG } from '../../constants';
 import { Order } from '../models';
 
 @Injectable()
 export class OrderService {
-  private orders: Record<string, Order> = {}
+  constructor(private readonly cartService: CartService) { }
 
-  findById(orderId: string): Order {
-    return this.orders[ orderId ];
+  async findById(orderId: string): Promise<Order> {
+    const client = new Client(PG_CLIENT_CONFIG);
+
+    await client.connect();
+
+    try {
+      const queryResult = await client.query({
+        text: `SELECT o.*, c.user_id FROM orders o JOIN carts c ON o.cart_id = c.id WHERE id = $1`,
+        values: [orderId]
+      });
+
+      const order = queryResult.rows?.[0];
+
+      if (order) {
+        const cart = await this.cartService.findByUserId(order.user_id)
+
+        return {
+          id: order.id,
+          userId: order.user_id,
+          cartId: cart.id,
+          items: cart.items,
+          payment: JSON.parse(order.payment),
+          delivery: JSON.parse(order.delivery),
+          comments: order.comments,
+          status: order.status,
+          total: order.total
+        };
+      }
+    }
+    finally {
+      await client.end();
+    }
   }
 
-  create(data: any) {
-    const id = v4(v4())
-    const order = {
-      ...data,
-      id,
-      status: 'inProgress',
-    };
+  async create(order: Order): Promise<Order> {
+    const client = new Client(PG_CLIENT_CONFIG);
 
-    this.orders[ id ] = order;
+    await client.connect();
 
-    return order;
+    try {
+      const status = 'inProgress';
+
+      const queryResult = await client.query({
+        text: `INSERT INTO orders (cart_id, payment, delivery, comments, status, total) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
+        values: [order.cartId, JSON.stringify(order.payment), JSON.stringify(order.delivery), order.comments, status, order.total]
+      });
+
+      return {
+        ...order,
+        id: queryResult.rows[0].id,
+        status: status
+      };
+    }
+    finally {
+      await client.end();
+    }
   }
 
-  update(orderId, data) {
-    const order = this.findById(orderId);
+  async update(orderId: string, data: Order): Promise<void> {
+    const order = await this.findById(orderId);
 
     if (!order) {
       throw new Error('Order does not exist.');
     }
 
-    this.orders[ orderId ] = {
-      ...data,
-      id: orderId,
+    const client = new Client(PG_CLIENT_CONFIG);
+
+    await client.connect();
+
+    try {
+      await client.query({
+        text: `UPDATE orders
+               SET payment = $2,
+                   delivery = $3,
+                   comments = $4,
+                   status = $5,
+                   total = $6
+               WHERE id = $1`,
+        values: [orderId, JSON.stringify(data.payment), JSON.stringify(data.delivery), data.comments, data.status, data.total]
+      });
+    }
+    finally {
+      await client.end();
     }
   }
 }
